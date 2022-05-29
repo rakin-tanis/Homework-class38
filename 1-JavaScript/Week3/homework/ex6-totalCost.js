ccd924049c2f2e1de815e7eb6645cfd5f6233706
akin tanis <akin.tanis.dev@gmail.com>
2022-05-30 01:31:16 +0200

    totalCost hw

--- 1-JavaScript/Week3/homework/ex6-totalCost.js
+++ 1-JavaScript/Week3/homework/ex6-totalCost.js
@@ -21,22 +21,30 @@ instead!
 3. Complete the unit test functions and verify that all is working as expected.
 -----------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+  'cheese': 4.99,
+  'honey': 5.49,
+  'yogurt': 3.79,
+  'Salmon': 24.99,
+  'bread': 1.99
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(cart) {
+  return Object.entries(cart)
+        .map(([key, value]) => value)
+        .reduce((prev, curr) => prev + curr, 0);
 }
 
 // ! Test functions (plain vanilla JavaScript)
 function test1() {
   console.log('\nTest 1: calculateTotalPrice should take one parameter');
-  // TODO replace this comment with your code
+  console.assert(calculateTotalPrice.length === 1);
 }
 
 function test2() {
   console.log('\nTest 2: return correct output when passed cartForParty');
-  // TODO replace this comment with your code
+  const expected = 4.99 + 5.49 + 3.79 + 24.99 + 1.99;
+  const actual = calculateTotalPrice(cartForParty);
+  console.assert(actual === expected);
 }
 
 function test() {
