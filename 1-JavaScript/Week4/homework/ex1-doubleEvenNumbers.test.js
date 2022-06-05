e1402bec58d32a92dce79830b5ebca91bf4fb0c6
akin tanis <akin.tanis.dev@gmail.com>
2022-06-05 11:17:36 +0200

    double event numbers function implementation and test

--- 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.test.js
+++ 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.test.js
@@ -12,13 +12,16 @@ Let's rewrite it (or _refactor_ it, as experienced developers would call it):
 // ! Function to be tested
 function doubleEvenNumbers(numbers) {
   // TODO rewrite the function body using `map` and `filter`.
-  const newNumbers = [];
+  /* const newNumbers = [];
   for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 === 0) {
       newNumbers.push(numbers[i] * 2);
     }
-  }
-  return newNumbers;
+  } */
+
+  return numbers
+    .filter(num => num % 2 === 0)
+    .map(even => even * 2);
 }
 
 // ! Unit test (using Jest)
