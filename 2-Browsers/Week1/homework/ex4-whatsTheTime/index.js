8995d2e12ad49ec51c2191f1850b809a83c9561f
akin tanis <akin.tanis.dev@gmail.com>
2022-06-11 16:56:13 +0200

    addCurrentTime code refactor

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -7,34 +7,35 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
   second). Use `setInterval()` to make sure the time stays current.
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
+let timeContainer;
+
 function addCurrentTime() {
+  if (!timeContainer) return;
+  const time = new Date();
+  const hours = format(time.getHours());
+  const minutes = format(time.getMinutes());
+  const seconds = format(time.getSeconds());
+  timeContainer.textContent = hours + ':' + minutes + ':' + seconds;
+}
+
+const format = (num) => {
+  return num < 10 ? '0' + num : num;
+};
+
+const initialize = () => {
   const style = `font-family: monospace, sans-serif;
                   font-size: 10em;
                   letter-spacing: 2px;
                   width: fit-content;
                   margin: auto;`;
-  const timeContainer = document.createElement('div');
+  timeContainer = document.createElement('div');
   timeContainer.style = style;
   document.body.appendChild(timeContainer);
-  document.body.parentElement.style.height = '100%'
+  document.body.parentElement.style.height = '100%';
   document.body.style.height = '100%';
   document.body.style.display = 'flex';
-  updateTime(timeContainer);
-}
+};
 
-function updateTime(container) {
-  const time = new Date();
-  const hours = format(time.getHours());
-  const minutes = format(time.getMinutes());
-  const seconds = format(time.getSeconds());
-  container.textContent = hours + ':' + minutes + ':' + seconds;
-  setInterval(() => {
-    updateTime(container);
-  }, 1000);
-}
+setInterval(addCurrentTime, 1000);
 
-function format(num) {
-  return num < 10 ? '0' + num : num;
-}
-
-window.addEventListener('DOMContentLoaded', addCurrentTime);
+window.addEventListener('DOMContentLoader', initialize());
