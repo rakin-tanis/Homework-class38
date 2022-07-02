d2784c0d9e005a6f74e480d56a01dfa4be579d91
akin tanis <akin.tanis.dev@gmail.com>
2022-07-02 19:30:52 +0200

    feature: ex3-rollDie

--- 3-UsingAPIs/Week1/homework/ex3-rollDie.js
+++ 3-UsingAPIs/Week1/homework/ex3-rollDie.js
@@ -11,48 +11,35 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/3-Usin
   explanation? Add your answer as a comment to be bottom of the file.
 ------------------------------------------------------------------------------*/
 
-// TODO Remove callback and return a promise
-function rollDie(callback) {
-  // Compute a random number of rolls (3-10) that the die MUST complete
-  const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
-  console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
+const rollDie = () =>
+  new Promise((resolve, reject) => {
+    const randomRollsToDo = Math.floor(Math.random() * 8) + 3;
+    console.log(`Die scheduled for ${randomRollsToDo} rolls...`);
 
-  const rollOnce = (roll) => {
-    // Compute a random die value for the current roll
-    const value = Math.floor(Math.random() * 6) + 1;
-    console.log(`Die value is now: ${value}`);
+    const rollOnce = (roll) => {
+      const value = Math.floor(Math.random() * 6) + 1;
+      console.log(`Die value is now: ${value}`);
 
-    // Use callback to notify that the die rolled off the table after 6 rolls
-    if (roll > 6) {
-      // TODO replace "error" callback
-      callback(new Error('Oops... Die rolled off the table.'));
-    }
+      if (roll > 6) {
+        return reject(new Error('Oops... Die rolled off the table.'));
+      }
+      if (roll === randomRollsToDo) {
+        return resolve(value);
+      }
+      if (roll < randomRollsToDo) {
+        setTimeout(() => {
+          return rollOnce(roll + 1);
+        }, 500);
+      }
+    };
 
-    // Use callback to communicate the final die value once finished rolling
-    if (roll === randomRollsToDo) {
-      // TODO replace "success" callback
-      callback(null, value);
-    }
-
-    // Schedule the next roll todo until no more rolls to do
-    if (roll < randomRollsToDo) {
-      setTimeout(() => rollOnce(roll + 1), 500);
-    }
-  };
-
-  // Start the initial roll
-  rollOnce(1);
-}
+    return rollOnce(1);
+  });
 
 function main() {
-  // TODO Refactor to use promise
-  rollDie((error, value) => {
-    if (error !== null) {
-      console.log(error.message);
-    } else {
-      console.log(`Success! Die settled on ${value}.`);
-    }
-  });
+  rollDie()
+    .then((res) => console.log(`Success! Die settled on ${res}.`))
+    .catch((error) => console.log(error.message));
 }
 
 // ! Do not change or remove the code below
@@ -60,3 +47,14 @@ if (process.env.NODE_ENV !== 'test') {
   main();
 }
 module.exports = rollDie;
+
+/* 
+Explanation:
+In the first version of the code even if the function throws an Error 
+it going on to run and in the end, it was printing success messages.
+
+But in the last version of the code, I used Promises. 
+After changing state of a promise from pending to resolved or rejected, 
+it doesn't change again. Therefore it does not print a success message 
+after throwing an error once.
+*/
